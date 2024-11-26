7fdd0feff32804d8f341e1854e4db1170ed13dbc
andrea-mestre <andrea.mestre@edu.univ-fcomte.fr>
2024-11-26 17:19:43 +0100

    referme les summary

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
-// *** ACCORDEON ***
+const accordeon = document.querySelector('.accordeon');
+const summary = accordeon.querySelector('summary');
+
+accordeon.querySelectorAll("summary").forEach((summary) => {
+    summary.addEventListener("click", () => {
+        accordeon.querySelectorAll("details").forEach((det) => 
+            (det.open = false));
+ });
+});
+    
